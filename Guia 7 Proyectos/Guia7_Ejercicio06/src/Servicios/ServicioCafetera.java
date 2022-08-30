e9b54816fe1de506663aaa9a1e18e616f8c5cc91
Franco Bussano <franbussano@gmail.com>
2022-08-30 19:47:36 -0300

    Correccion

--- Guia 7 Proyectos/Guia7_Ejercicio06/src/Servicios/ServicioCafetera.java
+++ Guia 7 Proyectos/Guia7_Ejercicio06/src/Servicios/ServicioCafetera.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servicios;
 
-/**
- *
- * @author Franco
- */
+import Entidades.Cafetera;
+import java.util.Scanner;
+
 public class ServicioCafetera {
-    
+
+    private final Scanner input = new Scanner(System.in).useDelimiter("\n");
+
+    public Cafetera LlenarCafetera() {
+
+        return null;
+
+    }
+
 }
