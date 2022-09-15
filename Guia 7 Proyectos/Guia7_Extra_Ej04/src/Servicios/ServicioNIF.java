88c858eeed1d05ab09bdf9a0550eb123141f3021
Franco Bussano <franbussano@gmail.com>
2022-09-15 19:52:51 -0300

    Do not enter pls

--- Guia 7 Proyectos/Guia7_Extra_Ej04/src/Servicios/ServicioNIF.java
+++ Guia 7 Proyectos/Guia7_Extra_Ej04/src/Servicios/ServicioNIF.java
@@ -8,17 +8,14 @@ public class ServicioNIF {
     private final Scanner input = new Scanner(System.in).useDelimiter("\n");
 
     public NIF CrearNif() {
-
         char[] vectorLetra = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
             'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-
         System.out.println("Generador de NIF...");
         System.out.println("Ingrese su DNI:");
         long dni = input.nextLong();
         int calculoLetra = (int) (dni % 23);
         char letraNif = vectorLetra[calculoLetra];
         return new NIF(dni, letraNif);
-
     }
 
     public void mostrar(NIF nif) {
